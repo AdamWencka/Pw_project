25f19fc772aad2f3ee49c117232ac73b6cd41fd1
Adam Wencka <adamwencka7@gmail.com>
2021-05-19 11:20:01 +0200

    Created UI for Popular Movies

--- MovieMVVMRX/app/src/main/java/com/example/moviemvvmrx/data/api/TheMovieDBInterface.kt
+++ MovieMVVMRX/app/src/main/java/com/example/moviemvvmrx/data/api/TheMovieDBInterface.kt
@@ -1,14 +1,19 @@
 package com.example.moviemvvmrx.data.api
 
 import com.example.moviemvvmrx.data.vo.MovieDetails
+import com.example.moviemvvmrx.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
     // https://api.themoviedb.org/3/movie/popular?api_key=5a209871c6aa3d5c1fadd206bbef9b1d&page=1
     // https://api.themoviedb.org/3/movie/299534?api_key=5a209871c6aa3d5c1fadd206bbef9b1d
     // https://api.themoviedb.org/3/
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page")page: Int): Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails> //single obeseverble
